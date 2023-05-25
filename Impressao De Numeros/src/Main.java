02aa57e5779831fa1d785285207de3cfc6fb3a25
Rafael <rafaelradieddine@gmail.com>
2023-05-25 16:01:29 -0300

    Initial commit

---
+++ Impressao De Numeros/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 10) {
+            count++;
+            System.out.println(count);
+        }
+    }
+}
